998cb7bdbbdeafa45b582c4cedda6dc323b0376f
Héctor Calderón Hervás <hector@evolutplatform.com>
2025-04-26 09:36:14 +0200

    Guardando cambios locales antes de rebase

---
+++ javadam/PrimerosPasos/src/EstructurasPrincipales.java
@@ -0,0 +1,44 @@
+
+public class EstructurasPrincipales {
+
+	public static void main(String[] args) {
+		/*Tipos de datos Ennteros:
+		 * 	int - 4 bytes.
+		 * 	short - 2 bytes.
+		 * 	long - 8 bytes.(Sufijo L).
+		 * 	byte - 1 byte.
+		 *Tipos Coma Flotante
+		 *	float - 4 bytes. (sufijo F). 6 o 7 cifras decimales.
+		 *	double - 8 bytes. 15 cifras decimales.
+		 *Tipo Caracter. ' '.
+		 *	char - Espaio en memoria no significativo. ' '.
+		 *Tipo boolean
+		 *	2 únicos valores - true / false.
+		 *
+		 *Variables - Espacio en la memoria del ordenador donde almacenamos un valor que podrá
+		 *	cambiar durante la ejecución del programa.
+		 *	- Crear o declarar: tipo_dato nombre; 
+		 *	- Inicializar (almacenar unn valor): variable = valor; tipo varaible = valor;
+		 *		Java es estático - El tipo declarado no va a poder cambiar.
+		 *		Java es fuertemente tipado - No se permiten operaciones entre tipos diferentes.
+		 *			Para poder operar habrá que hacer conversiones explícitas.
+		 *		Java no permite utilizar variables no inicializadas.
+		 *
+		 *Inferencia de tipos: var variable = valor; 
+		 *	Deducir automáticamente el tipo de una variable.
+		 *	El compilador analiza el valor y le asigna el tipo más adecuado.
+		 *	Utiliza los tipos comodines (int, double).
+		 *	La variable no podrá cambiar su tipo durante la ejecución del programa.
+		 *	Se puede usar en:
+		 *		- Variables locales dentro de métodos.
+		 *		- En bucles for-each for.
+		 *		- En try-with resources.
+		 *	No se puede usar:
+		 *		- Parámetros de métodos.
+		 *		- Variables de clase o atributos de instancia.
+		 *		- Tipos genéricos.
+		 * */
+
+	}
+
+}
